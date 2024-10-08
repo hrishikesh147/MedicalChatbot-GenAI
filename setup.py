952cf2bc3713955e52dfda5f0145976640a0451c
hrishikesh147 <hrishikeshbhagawati@gmail.com>
2024-10-08 16:48:56 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages,setup
+
+setup(name="Gen-AI-Project1",
+      version="0.0.1",
+      description="A Gen-AI-Project ",
+      packages=find_packages(),
+      install_requires=[]
+      )
